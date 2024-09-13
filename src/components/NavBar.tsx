9328eb34edfdcfe6ae051a1e325a552478f32860
mohsenbahrami64 <mohsenbahrami64@gmail.com>
2024-09-13 20:58:57 +0330

    impelement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import Logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-const NavBar = () => {
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch }: Props) => {
   return (
     <div>
       <HStack>
         <Image src={Logo} boxSize="60px" />
-        <SearchInput />
+        <SearchInput onSearch={onSearch} />
         <ColorModeSwitch />
       </HStack>
     </div>
